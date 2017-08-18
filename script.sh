c8b1cc4ed6763fdee4a144617027b83a29904ee1
Craig J Smith <nctiggy@gmail.com>
2017-08-18 11:00:02 -0700

    updated docker-compose to include enabling graphite and update line breaks in script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-apt install -y apt-transport-https ca-certificates curl \
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu " \
-    "$(lsb_release -cs) stable"
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
 apt update -y
 apt install -y docker-ce
-curl -o /usr/local/bin/docker-compose -L \
-    "https://github.com/docker/compose/releases/download/1.11.2/docker-compose"\
-    "-$(uname -s)-$(uname -m)"
+curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)"
 chmod +x /usr/local/bin/docker-compose
-mkdir /var/lib/paclabs_ && sudo cp /tmp/jenkins-compose.yml /var/lib/paclabs_
+mkdir /var/lib/paclabs_ && sudo cp /tmp/docker-compose.yml /var/lib/paclabs_
 docker-compose -f /var/lib/paclabs_/docker-compose.yml up -d
